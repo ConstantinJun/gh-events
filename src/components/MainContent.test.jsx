fc8424740bdacfe36f9ae97446c90e1d8a445c54
Constantin <bunuconstantinjun@gmail.com>
2023-10-06 16:01:34 +0300

    add comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-// ad comments
+// ad commentss
 
 describe('MainContent', () => {
   it('should render a button', () => {
